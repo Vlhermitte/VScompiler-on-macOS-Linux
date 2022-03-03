e7f66e91e9e1757a15bede81fc8e9e7df1ae80d6
Valentin Lhermitte <79002719+etu47072@users.noreply.github.com>
2022-03-03 15:49:36 +0100

    Fixing bugs when installing wine on Linux

--- installation.sh
+++ installation.sh
@@ -21,4 +21,23 @@ then
   fi
     echo 'installing mingw-w64'
     brew install mingw-w64;
+
+elif [[ $OSTYPE == 'linux-gnu' ]]; then
+    echo 'OSTYPE : linux'
+
+    echo 'installing mingw-w64'
+    sudo apt install mingw-w64;
+
+    if which 'wine' ; then
+        echo 'Wine is already installed'
+    else
+      echo 'Wine is not installed you need to install it'
+
+      sudo dpkg --add-architecture i386;
+      sudo apt update;
+
+      sudo apt install wine64 wine32;
+
+      sudo apt install --install-recommends winehq-stable;
+    fi
 fi
